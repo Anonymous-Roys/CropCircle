5935a9d2bc6677ae1c02359b112eb3616621ab6c
jow342wise <jnyarko342@gmail.com>
2024-11-13 11:14:21 +0000

    Update database settings to comment out SQLite configuration

--- server/farmsales/settings.py
+++ server/farmsales/settings.py
@@ -73,16 +73,16 @@ WSGI_APPLICATION = 'farmsales.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'CropCircle',
-        'USER': 'backend_team',
-        'PASSWORD': 'Akogo660221.',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'CropCircle',
+#         'USER': 'backend_team',
+#         'PASSWORD': 'Akogo660221.',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
